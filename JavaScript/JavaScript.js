9551b222d6bb1f9e8bcca1f299dfbb721237cbb0
unknown <n.odedara7@gmail.comm>
2017-11-23 19:00:16 +0000

    updated my website

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,14 @@
+//Get select object
+var objSelect = document.getElementById("Size");
+
+//Set selected
+setSelectedValue(objSelect, "10");
+
+function setSelectedValue(selectObj, valueToSet) {
+    for (var i = 0; i < selectObj.options.length; i++) {
+        if (selectObj.options[i].text== valueToSet) {
+            selectObj.options[i].selected = true;
+            return;
+        }
+    }
+}
